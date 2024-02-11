0b699e8580979446016f0886edd4b6330bd74867
vitolaman <vito99varianlaman@gmail.com>
2024-02-12 06:25:52 +0700

    chore: remove console log

--- src/layout/navbar.tsx
+++ src/layout/navbar.tsx
@@ -3,7 +3,6 @@
 import { logodrizy } from '~/images';
 
 const Navbar = () => {
-  console.log(logodrizy);
   return (
     <nav className='fixed top-0 z-10 flex w-full items-center bg-white py-10 shadow-xl md:px-[80px]'>
       <div className='container mx-auto flex items-center justify-between px-4'>
