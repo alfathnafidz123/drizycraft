b1c055de8a9763effa008cb1c3bf5af46c3fee53
vitolaman <vito99varianlaman@gmail.com>
2024-02-28 13:15:25 +0700

    feat: slicing create password page

--- public/images/index.ts
+++ public/images/index.ts
@@ -24,13 +24,14 @@ import loginImage from './login-image.png';
 import newMember from './newMember.png';
 import pintCrafter from './pintCrafter.png';
 import project1 from './project-1.png';
-import projectSign from './projectSign.png';
-import projectSubscribe from './projectSubscribe.png';
-import projectShare from './projectShare.png';
-import projectStars from './projectStars.png';
-import projectLike from './projectLike.png';
-import projectShare1 from './projectShare1.png';
+import projectGrid from './projectGrid.png';
 import projectImage from './projectImage.png';
+import projectLike from './projectLike.png';
+import projectShare from './projectShare.png';
+import projectShare1 from './projectShare1.png';
+import projectSign from './projectSign.png';
+import projectStars from './projectStars.png';
+import projectSubscribe from './projectSubscribe.png';
 import projectUpload from './projectUpload.png';
 import sale from './sale.png';
 import saleProduct from './sale-product.png';
@@ -68,13 +69,14 @@ export {
   newMember,
   pintCrafter,
   project1,
+  projectGrid,
   projectImage,
-  projectShare,
-  projectSign,
-  projectSubscribe,
-  projectStars,
   projectLike,
+  projectShare,
   projectShare1,
+  projectSign,
+  projectStars,
+  projectSubscribe,
   projectUpload,
   sale,
   saleProduct,
