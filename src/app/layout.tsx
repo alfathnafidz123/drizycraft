d391b13eb8b32167a668e42097a81dc079015b38
dionisius7 <150808442+dionisius7@users.noreply.github.com>
2024-12-03 18:20:16 +0700

    fix: remove suspense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ import '@/styles/colors.css';
 import 'react-toastify/dist/ReactToastify.css';
 
 import Loading from '@/app/loading';
-const StoreProvider = lazy(() => import('@/app/StoreProvider'));
+import StoreProvider from '@/app/StoreProvider';
 import { siteConfig } from '@/constant/config';
 import AsyncCSSSlick from '@/layout/asyncCssSlick';
 import AsyncCSSThemeSlick from '@/layout/asyncCssThemeSlick';
@@ -193,21 +193,19 @@ export default function RootLayout({
 
       <body>
         <SpeedInsights />
-        <Suspense fallback={<Loading />}>
-          <StoreProvider>
-            {/* <ComingSoonModal /> */}
-            <Suspense fallback={null}>
-              <Navbar />
-            </Suspense>
-            <Suspense fallback={<Loading />}>
-              {children}
-            </Suspense>
-            <ToastContainer />
-            <Suspense fallback={null}>
-              <Footer />
-            </Suspense>
-          </StoreProvider>
-        </Suspense>
+        <StoreProvider>
+          {/* <ComingSoonModal /> */}
+          <Suspense fallback={null}>
+            <Navbar />
+          </Suspense>
+          <Suspense fallback={<Loading />}>
+            {children}
+          </Suspense>
+          <ToastContainer />
+          <Suspense fallback={null}>
+            <Footer />
+          </Suspense>
+        </StoreProvider>
       </body>
       <GoogleAnalytics gaId='G-S80R5B2E8S' />
     </html>
